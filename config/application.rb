acecdbf7582f6a858cfcb8ebdb5d7b35e3a1d1aa
Maria Guarecuco <mgabriela.guarecuco@gmail.com>
2020-04-26 20:32:12 -0400

    Move CORS config to initializer

--- config/application.rb
+++ config/application.rb
@@ -35,12 +35,5 @@ module LuminarlyBackend
     config.api_only = true
 
     config.middleware.use Rack::Attack
-
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
-      end
-    end
   end
 end
